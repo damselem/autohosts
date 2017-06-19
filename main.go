b14708cfa0c606c17b8cfbd03303f66f260d648c
Daniel Salmeron Amselem <daniel.amselem@gmail.com>
2017-06-19 13:59:57 +0300

    Execute AWS and GCP providers concurrently

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"github.com/damselem/autohosts/cmd"
-	kingpin "gopkg.in/alecthomas/kingpin.v2"
 	"log"
 	"os"
+	"sync"
+
+	"github.com/damselem/autohosts/cmd"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 func main() {
@@ -26,8 +28,37 @@ func main() {
 	case "gcp":
 		err = cmd.RunGCPCommand(*dstFile, *gcpProjects)
 	case "all":
-		err = cmd.RunAWSCommand(*dstFile)
-		err = cmd.RunGCPCommand(*dstFile, *allGcpProjects)
+		var wg sync.WaitGroup
+		errsCh := make(chan error)
+
+		wg.Add(1)
+		go func(dstFile string) {
+			if err := cmd.RunAWSCommand(dstFile); err != nil {
+				errsCh <- err
+				return
+			}
+
+			wg.Done()
+		}(*dstFile)
+
+		wg.Add(1)
+		go func(dstFile string, projects []string) {
+			if err := cmd.RunGCPCommand(dstFile, projects); err != nil {
+				errsCh <- err
+				return
+			}
+
+			wg.Done()
+		}(*dstFile, *allGcpProjects)
+
+		go func() {
+			for cloudErr := range errsCh {
+				err = cloudErr
+				wg.Done()
+			}
+		}()
+
+		wg.Wait()
 	}
 
 	if err != nil {
